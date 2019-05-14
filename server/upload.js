013dacffd5f6b22cf662b470617d9c45bdd97e0a
unknown <heagueron@gmail.com>
2019-05-14 10:27:11 -0400

    front started

--- server/upload.js
+++ server/upload.js
@@ -10,11 +10,14 @@ module.exports = function upload(req, res) {
 
     var form = new IncomingForm()
 
+    // Set the directory for placing file uploads in
+    form.uploadDir = "C:/Users/USUARIO/file_up/uploaded";
+
     // The first callback is called for every file in the form
-    form.on('file', (field, file) => {
+    form.on('file', (field, file) => { // Emitted whenever a field / file pair has been received
         // Do something with the file
         // e.g. save it to the database
-        // you can access it using file.path
+        // we can access it using file.path
     })
 
     // The second callback is called when the form is completely parsed
@@ -22,7 +25,11 @@ module.exports = function upload(req, res) {
         res.json()
     }) 
 
-    // We trigger the parsing of the form using
+    form.on('error', function(err) { // Emitted when there is an error processing the incoming form
+        console.log(`Error receiving the file ... ${err}`)
+    });
+    
+    // Trigger the parsing of the form
     form.parse(req)
     
 }
